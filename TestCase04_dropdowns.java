b1849b8c852d5c8e54898bf9dd6627da7eaa9d6c
artur <arturo.babenko@gmail.com>
2020-06-10 13:19:07 +0200

    some adittional notice

--- TestCase04_dropdowns.java
+++ TestCase04_dropdowns.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -12,20 +14,46 @@ public class TestCase04_dropdowns {
         System.setProperty("webdriver.chrome.driver", "//Users//arthurbabenko//" +
                 "Documents//Testing//selenium-java-3//webDriver//chrome//chromedriver");
         WebDriver chrome = new ChromeDriver();
-        chrome.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        //chrome.get("https://rahulshettyacademy.com/dropdownsPractise/");
         /*
-        chrome.findElement(By.cssSelector("ctl00_mainContent_DropDownListCurrency")).click();
         Select s = new Select(chrome.findElement(By.id("ctl00$mainContent$DropDownListCurrency")));
         s.selectByValue("USD");
         s.selectByIndex(3);
          */
 
+        /*
         chrome.findElement(By.id("divpaxinfo")).click();
         Thread.sleep(2000);
         for (int i = 1; i < 10; i++) {
             chrome.findElement(By.id("hrefIncAdt")).click();
         }
         chrome.findElement(By.id("btnclosepaxoption")).click();
-    }
+         */
 
+        //Dynamic dropdown
+        /*
+        chrome.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        chrome.findElement(By.xpath("//a[@value='DEL']")).click();
+        Thread.sleep(2000);
+        //chrome.findElement(By.xpath("(//a[@value='GOI'])[2]")).click(); //shorter navigation by index
+
+        chrome.findElement(By.xpath("(//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'])" +
+                "//a[@value='GOI']")).click();
+        chrome.close();
+         */
+
+        //AutoSuggestive
+
+        chrome.get("https://www.makemytrip.com");
+        WebElement repeat = chrome.findElement(By.id("fromCity"));
+        repeat.click();
+        repeat.sendKeys("mum");
+
+        WebElement repeatTo = chrome.findElement(By.id("toCity"));
+        repeatTo.click();
+        repeatTo.sendKeys("del");
+        repeatTo.sendKeys(Keys.ARROW_DOWN);
+        repeatTo.sendKeys(Keys.ENTER);
+
+    }
 }
