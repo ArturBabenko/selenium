00596984ee325d747125153ea307723c6acf924f
artur <arturo.babenko@gmail.com>
2020-06-11 11:32:27 +0200

    new Assignment

--- Temporary.java
+++ Temporary.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 
@@ -43,7 +48,7 @@ public class Temporary {
  */
 
         // Assignment 2
-
+/*
         chrome.get("https://www.cleartrip.com/");
 
         Select s = new Select(chrome.findElement(By.id("Adults")));
@@ -63,7 +68,52 @@ public class Temporary {
         chrome.findElement(By.id("SearchBtn")).click();
         System.out.println(chrome.findElement(By.id("homeErrorMessage")).getText());
 
+ */
 
+        //Assignment 3
+        /*
+        WebDriverWait wdw = new WebDriverWait(chrome, 30);
+        chrome.get("http://www.itgeared.com/demo/1506-ajax-loading.html");
+        chrome.findElement(By.xpath("//a[contains(text(),'Click to load get data via Ajax!')]")).click();
+        wdw.until(ExpectedConditions.visibilityOfElementLocated(By.id("results")));
+        System.out.println(chrome.findElement(By.id("results")).getText());
+        chrome.close();
+         */
+
+        //Assignment 4
+        /*
+        chrome.get("https://the-internet.herokuapp.com/windows");
+
+        chrome.findElement(By.xpath("//a[contains(text(),'Click Here')]")).click();
+        Set<String> windows = chrome.getWindowHandles();
+        Iterator<String> iter = windows.iterator();
+        String parrent = iter.next();
+        String chield = iter.next();
+        chrome.switchTo().window(chield);
+        System.out.println(chrome.findElement(By.cssSelector("h3")).getText());
+        chrome.switchTo().window(parrent);
+        System.out.println(chrome.findElement(By.xpath("//h3[contains(text(),'Opening a new window')]")).getText());
+        chrome.close();
+         */
+
+        // another construction of switching
+        /*
+        Set<String> abc=driver.getWindowHandles();
+        Iterator<String> it=abc.iterator();
+        String parentWindow=it.next();
+        driver.switchTo().window(it.next());
+        System.out.println(driver.findElement(By.xpath("//div/h3")).getText());
+        driver.switchTo().window(parentWindow);
+        System.out.println();
+         */
+
+        //Assignment 5
+
+        chrome.get("https://the-internet.herokuapp.com/");
+        chrome.findElement(By.xpath("//a[contains(text(),'Nested Frames')]")).click();
+        chrome.switchTo().frame("frame-top").switchTo().frame("frame-middle");
+        System.out.println(chrome.findElement(By.id("content")).getText());
+        chrome.close();
 
     }
 }
